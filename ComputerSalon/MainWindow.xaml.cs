1d04541efa8edc3bc6cfde31175ba22dcee5ec69
206922 <206922@KB-120-C03>
2023-03-24 09:33:45 +0300

    Добавьте файлы проекта.

---
+++ ComputerSalon/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Windows.Markup;
+
+namespace ComputerSalon
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.IsEnabled = true;
+            timer.Tick += (o, t) => { TimeBlock.Text = DateTime.Now.ToString(); };
+            timer.Start();
+            Closing += WindowClosing;
+        }
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            const string message = "Вы уверены, что хотите закрыть приложение?";
+            const string caption = "Form closing";
+            var result = MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}

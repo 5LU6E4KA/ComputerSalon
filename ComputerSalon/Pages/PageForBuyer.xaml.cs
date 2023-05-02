45f995271a58b99348c14818192953f1e41eb587
Дмитрий Виталев <206920@edu.fa.ru>
2023-05-02 14:51:34 +0300

    PR 21-22

--- ComputerSalon/Pages/PageForBuyer.xaml.cs
+++ ComputerSalon/Pages/PageForBuyer.xaml.cs
@@ -31,8 +31,41 @@ namespace ComputerSalon.Pages
                 Price = x.CharacteristicsForPC.Price + x.Monitors.Price
             }).ToList();
             ListProducts.ItemsSource = currents;
+            Filter.SelectedIndex = 0;
         }
 
-       
+        private void ClearFilter_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxSearch.Text = "";
+            Filter.SelectedIndex = 0;
+        }
+
+        private void Filter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAssembly();
+        }
+
+        private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAssembly();
+        }
+
+        private void UpdateAssembly()
+        {
+            var currents = ComputerSalonDB.Context.Assembling.ToList().Select(x => new
+            {
+                Name = x.Name,
+                Quantity = x.CharacteristicsForPC.Quantity,
+                Photo = x.Photo,
+                Price = x.CharacteristicsForPC.Price + x.Monitors.Price
+            }).ToList();
+
+            currents = currents.Where(x => x.Name.ToLower().Contains(TextBoxSearch.Text.ToLower())).ToList();
+
+            if (Filter.SelectedIndex == 0)
+                ListProducts.ItemsSource = currents.OrderBy(x => x.Name).ToList();
+            else ListProducts.ItemsSource = currents.OrderByDescending(x => x.Name).ToList();
+
+        }
     }
 }
